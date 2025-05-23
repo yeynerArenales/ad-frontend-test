a8991dad64ca10c3630dc16b9596b087195eab77
Yeyner Arenales <yeyner.arenales0811@gmail.com>
2025-05-23 18:29:49 -0500

    chore: add Readonly type to layout props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <body className={`${inter.className} min-h-screen flex flex-col`}>
