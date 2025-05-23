4822a485236dab8e6bdbef18908a32ff20b7d34d
Yeyner Arenales <yeyner.arenales0811@gmail.com>
2025-05-23 17:55:25 -0500

    refactor: improve header structure and responsive padding

--- src/components/organisms/header/index.tsx
+++ src/components/organisms/header/index.tsx
@@ -3,13 +3,16 @@ import Image from "next/image";
 
 export const Header = () => {
   return (
-    <header className="bg-surface px-20 py-2.5 flex items-center justify-between">
-      <Link href="/" className="font-bold text-2xl text-text-gray">
-        GamerShop
-      </Link>
-      <Link href="/cart">
-        <Image src="/icons/cart.svg" alt="cart" width={24} height={24} />
-      </Link>
+    <header>
+      <nav className="bg-surface px-4 md:px-20 py-2.5 flex items-center justify-between">
+        <Link href="/" className="font-bold text-2xl text-text-gray">
+          GamerShop
+        </Link>
+
+        <Link href="/cart">
+          <Image src="/icons/cart.svg" alt="cart" width={24} height={24} />
+        </Link>
+      </nav>
     </header>
   );
 };
